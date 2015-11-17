018ad1286b506dc882308ff0b9c09d47cfa2a2df
SunguochaoYeepay <sunguochao@gmail.com>
2015-11-17 14:06:43 +0800

    增加nodemon 提高变成效率 nodemon appName

--- nodeSNS/routes/index.js
+++ nodeSNS/routes/index.js
@@ -3,7 +3,35 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '首页' });
+    console.log('首页')
 });
+
+/* GET home page. */
+router.post('/post', function(req, res, next) {
+    res.render('post', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/reg', function(req, res, next) {
+    res.render('reg', { title: '注册' });
+});
+
+
+/* GET home page. */
+router.get('/login', function(req, res) {
+    res.render('login', { title: '登录成功！' });
+});
+
+/* GET home page. */
+router.get('/logout', function(req, res, next) {
+    res.render('logout', { title: '退出！' });
+});
+
+
+
+
+
+
 
 module.exports = router;
--- nodeSNS/routes/users.js
+++ nodeSNS/routes/users.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:user', function(req, res, next) {
+//  res.send('respond with a resource');
+    res.render('login', {
+        title: '用户登录',
+        name: '国超'
+    });
+    console.log(name)
 });
+
+
+
+
 
 module.exports = router;
